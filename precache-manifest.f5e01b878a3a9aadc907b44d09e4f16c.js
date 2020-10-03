f20b6940e7554dd6c3a73ce1fa0e8fb505098c48
PRAVEENKUMARPATNALA <patnalapraveenkumar@gmail.com>
2020-10-03 18:15:06 +0530

    Updates

---
+++ precache-manifest.f5e01b878a3a9aadc907b44d09e4f16c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b2797bb29494d06b59dcdb712f7120b",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "7a801b245ce33bfc35c1",
+    "url": "/react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2e2154c8b2d2a02e1a13",
+    "url": "/react/static/js/2.561569e4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react/static/js/2.561569e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a801b245ce33bfc35c1",
+    "url": "/react/static/js/main.3ee9f537.chunk.js"
+  },
+  {
+    "revision": "7a236b3df9a4d49c2bb6",
+    "url": "/react/static/js/runtime-main.4ddefbe0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react/static/media/logo.5d5d9eef.svg"
+  }
+]);
